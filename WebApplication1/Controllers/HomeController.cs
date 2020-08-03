d487fe3f793d189eb08f6d4d6303d2204f4fcae8
diwaschaulagain <chaulagaindiwas@yahoo.com>
2020-08-03 11:34:32 +0545

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication1.Models;
+using WebApplication1.Models.ViewModel;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+       
+     
+    }
+}
